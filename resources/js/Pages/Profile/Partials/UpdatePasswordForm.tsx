c637a1f9445d6e9d2b7b31fed1b3e6f946d65387
mochacino <lanaksa28@gmail.com>
2024-08-01 13:24:40 +0700

    deactivated update password

--- resources/js/Pages/Profile/Partials/UpdatePasswordForm.tsx
+++ resources/js/Pages/Profile/Partials/UpdatePasswordForm.tsx
@@ -19,21 +19,21 @@ export default function UpdatePasswordForm({ className = '' }: { className?: str
     const updatePassword: FormEventHandler = (e) => {
         e.preventDefault();
 
-        put(route('password.update'), {
-            preserveScroll: true,
-            onSuccess: () => reset(),
-            onError: (errors) => {
-                if (errors.password) {
-                    reset('password', 'password_confirmation');
-                    passwordInput.current?.focus();
-                }
-
-                if (errors.current_password) {
-                    reset('current_password');
-                    currentPasswordInput.current?.focus();
-                }
-            },
-        });
+        // put(route('password.update'), {
+        //     preserveScroll: true,
+        //     onSuccess: () => reset(),
+        //     onError: (errors) => {
+        //         if (errors.password) {
+        //             reset('password', 'password_confirmation');
+        //             passwordInput.current?.focus();
+        //         }
+        //
+        //         if (errors.current_password) {
+        //             reset('current_password');
+        //             currentPasswordInput.current?.focus();
+        //         }
+        //     },
+        // });
     };
 
     return (
@@ -42,7 +42,7 @@ export default function UpdatePasswordForm({ className = '' }: { className?: str
                 <h2 className="text-lg font-medium text-gray-900">Update Password</h2>
 
                 <p className="mt-1 text-sm text-gray-600">
-                    Ensure your account is using a long, random password to stay secure.
+                    Update password feature has been deactivated.
                 </p>
             </header>
 
@@ -51,12 +51,13 @@ export default function UpdatePasswordForm({ className = '' }: { className?: str
                     <InputLabel htmlFor="current_password" value="Current Password" />
 
                     <TextInput
+                        disabled
                         id="current_password"
                         ref={currentPasswordInput}
                         value={data.current_password}
                         onChange={(e) => setData('current_password', e.target.value)}
                         type="password"
-                        className="mt-1 block w-full"
+                        className="mt-1 block w-full bg-gray-200"
                         autoComplete="current-password"
                     />
 
@@ -67,12 +68,13 @@ export default function UpdatePasswordForm({ className = '' }: { className?: str
                     <InputLabel htmlFor="password" value="New Password" />
 
                     <TextInput
+                        disabled
                         id="password"
                         ref={passwordInput}
                         value={data.password}
                         onChange={(e) => setData('password', e.target.value)}
                         type="password"
-                        className="mt-1 block w-full"
+                        className="mt-1 block w-full bg-gray-200"
                         autoComplete="new-password"
                     />
 
@@ -83,11 +85,12 @@ export default function UpdatePasswordForm({ className = '' }: { className?: str
                     <InputLabel htmlFor="password_confirmation" value="Confirm Password" />
 
                     <TextInput
+                        disabled
                         id="password_confirmation"
                         value={data.password_confirmation}
                         onChange={(e) => setData('password_confirmation', e.target.value)}
                         type="password"
-                        className="mt-1 block w-full"
+                        className="mt-1 block w-full bg-gray-200"
                         autoComplete="new-password"
                     />
 
